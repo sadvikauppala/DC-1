a70d661649eadca7923570a910d501e66b7b2149
uppala sadvika <uppalasadvika06@gmail.com>
2025-03-12 10:20:19 +0530

    done

--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -3,15 +3,15 @@ import React from "react";
 
 const Usercard = ()=>{
     const profilePhoto = "./src/assets/myphoto.jpg";
-     const name = "Sarayu";
-     const email = "sarayu2926@gmail.com";
-     const phone = "99999999999";
+     const name = "Sadvika";
+     const email = "uppalasadvika06@gmail.com";
+     const phone = "9256944379";
      const address = "1234 Main St,City ,State,1234";
 
 
 return(
     <div>
-        <img src={progilephoto}/>
+        <img src={profilePhoto}/>
         <h2>{name}</h2>
         <p>{email}</p>
         <p>{phone}</p>
